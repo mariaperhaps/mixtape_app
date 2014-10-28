12ae53828cafd198be5227d887243a0cf0374800
mariaperhaps <mariatschettino@gmail.com>
2014-10-28 18:31:04 -0400

    added to config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.assets.compress = true
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = ‘X-Accel-Redirect’
+  config.assets.compile = true
+
 end
